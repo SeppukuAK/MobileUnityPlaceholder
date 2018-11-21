58c82a1fd9eab20f2e0e607d0cab32316b754e74
Raúl Serrano Gómez <raulserranodev@gmail.com>
2018-11-21 11:53:41 +0100

    First commit

---
+++ Assets/SalirYVolver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalirYVolver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+#if !UNITY_ANDROID
+        Destroy(this.gameObject);
+#endif
+
+    }
+
+#if UNITY_ANDROID
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit(); //Se carga la aplicación :(
+                                //Hay que ser amables :(
+        }
+    }
+#endif
+
+}
